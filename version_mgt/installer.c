f7911e339ff8c3228c4c5e301dabd73480dbda3b
syltr1x <syltr1x@proton.me>
2024-04-19 14:37:23 -0300

    fix: installer url

--- version_mgt/installer.c
+++ version_mgt/installer.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
 int main() {
-    int result = system("powershell -c \"iwr -useb https://raw.githubusercontent.com/syltr1x/ICS/main/installer.ps1 | iex\"");
+    int result = system("powershell -c \"iwr -useb https://raw.githubusercontent.com/syltr1x/ICS/main/version_mgt/installer.ps1 | iex\"");
     return 0;
 }

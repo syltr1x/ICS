c5d3964e4a0da412b282925910e51077d9f82381
syltr1x <syltr1x@proton.me>
2024-04-19 14:37:47 -0300

    fix: updater url

--- version_mgt/updater.c
+++ version_mgt/updater.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
 int main() {
-    int result = system("powershell -c \"iwr -useb https://raw.githubusercontent.com/syltr1x/ICS/main/updater.ps1 | iex\"");
+    int result = system("powershell -c \"iwr -useb https://raw.githubusercontent.com/syltr1x/ICS/main/version_mgt/updater.ps1 | iex\"");
     return 0;
 }
